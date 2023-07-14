08e7f0982ce16b141b931dd1549d2e88d061db59
wishba <wisnubayu34@gmail.com>
2023-07-14 09:14:30 +0700

    history counter

--- scripts/history.js
+++ scripts/history.js
@@ -1,15 +1,23 @@
 const history = document.getElementById('history')
 
-for (let i = 0; i < localStorage.length; i++) {
-  const key = localStorage.key(i)
-  const value = localStorage.getItem(key)
-
-  let link = document.createElement('a')
-  link.href = value
-  link.textContent = key
-
+if (localStorage.length == 0) {
   let list = document.createElement('li')
-  list.appendChild(link)
-
+  list.textContent = 'Anda belum membaca artikel'
   history.appendChild(list)
+}
+
+if (localStorage.length >= 1) {
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i)
+    const value = localStorage.getItem(key)
+
+    let link = document.createElement('a')
+    link.href = value
+    link.textContent = key
+
+    let list = document.createElement('li')
+    list.appendChild(link)
+
+    history.appendChild(list)
+  }
 }
---
+++ scripts/historyCount.js
@@ -0,0 +1,1 @@
+document.getElementById('historyCount').innerText = localStorage.length
